5364d81f618319cb87cc250d57c75acefb736596
Cleon DSouza <cleondsouza@gmail.com>
2025-08-02 16:22:38 +0530

    Top K Frequent Element using Bucket Sort

--- Arrays & Hashing/Medium/TopKFrequentElements.java
+++ Arrays & Hashing/Medium/TopKFrequentElements.java
@@ -10,67 +10,40 @@
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Build frequency map
+        // Step 1: Count frequencies
         Map<Integer, Integer> frequencyMap = new HashMap<>();
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        // Step 2: Convert map to 2D array: [num, frequency]
-        int size = frequencyMap.size();
-        int[][] freqArray = new int[size][2];
-        int index = 0;
+        // Step 2: Create buckets - array of lists where index = frequency
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int i = 0; i <= nums.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        // Step 3: Fill the buckets
         for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            freqArray[index][0] = entry.getKey();   // number
-            freqArray[index][1] = entry.getValue(); // frequency
-            index++;
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            buckets[freq].add(num);
         }
 
-        // Step 3: Sort using quicksort in descending order of frequency
-        quickSort(freqArray, 0, size - 1);
-
-        // Step 4: Extract top k frequent elements
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = freqArray[i][0]; // top k elements by frequency
-        }
-
-        return result;
-    }
-
-    private void quickSort(int[][] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    private int partition(int[][] arr, int low, int high) {
-        int pivotFreq = arr[high][1];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j][1] >= pivotFreq) { 
-                i++;
-                swap(arr, i, j);
+        // Step 4: Collect top k frequent elements
+        List<Integer> result = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0 && result.size() < k; i--) {
+            for (int num : buckets[i]) {
+                result.add(num);
+                if (result.size() == k) break;
             }
         }
 
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
+        // Convert list to array
+        int[] topK = new int[k];
+        for (int i = 0; i < k; i++) {
+            topK[i] = result.get(i);
+        }
 
-    private void swap(int[][] arr, int i, int j) {
-        int tempNum = arr[i][0];
-        int tempFreq = arr[i][1];
-
-        arr[i][0] = arr[j][0];
-        arr[i][1] = arr[j][1];
-
-        arr[j][0] = tempNum;
-        arr[j][1] = tempFreq;
+        return topK;
     }
 }
-
